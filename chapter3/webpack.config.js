569b56747f18c809fec8558c38892203954a5124
lijunyi01 <lijunyi01@gmail.com>
2018-05-19 23:03:32 +0800

    presets: ['es2015'] -> presets: ['env'],

--- chapter3/webpack.config.js
+++ chapter3/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015'],
+          presets: ['env'],
           plugins: ['transform-runtime']
         }
       }
